5df8bdb0387c3ee7f672e7d91a8cfa482906112a
Thiago Nakamura <thiagokenzo@outlook.com>
2025-04-07 15:19:41 -0300

    feat: add BankAccount entity with deposit and withdraw operations

---
+++ src/model/entities/BankAccount.java
@@ -0,0 +1,59 @@
+package model.entities;
+
+
+/**
+ * Represents a simple bank account with basic deposit and withdrawal functionality.
+ */
+public class BankAccount {
+
+	// The name of the account holder
+	private String holder;
+	
+	// The current balance of the account
+	private Double balance;
+	
+	public BankAccount(String holder, Double balance) {
+		super();
+		this.holder = holder;
+		this.balance = balance;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+	
+	// Deposits a given amount into the account.
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	
+	// Withdraws a given amount from the account, if sufficient balance exists.
+	public void withdraw(double amount) {
+		if(amount > balance) {
+			System.out.println("Not enough balance!");
+		}
+		else {
+			balance -= amount;
+		}
+	}
+
+	
+	// Returns a string representation of the account's balance.
+	@Override
+	public String toString() {
+		return "Balance: $" + String.format("%.2f", balance);
+	}	
+}
